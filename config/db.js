d61dea504ade1698114a1c26a209930b35784355
tandavala <jose.tandavala@gmail.com>
2020-01-01 20:08:05 +0100

    conectado no banco de dados

--- config/db.js
+++ config/db.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose')
 const config = require('config')
 const db = config.get("mongoRemote")
 
+
 const connectDB = async () => {
+    console.log('before calling...')
     try {
         await mongoose.connect(db, {
             useUnifiedTopology: true,
@@ -13,8 +15,11 @@ const connectDB = async () => {
         console.log("MongoDB Connected...")
     } catch (err) {
         console.log(err.message)
+
         process.exit(1)
     }
+    console.log('after try...')
 }
+
 
 module.exports = connectDB
