3892e01d5e25c3758c62aae9b53f114c62f0bca4
tandavala <jose.tandavala@gmail.com>
2020-01-01 20:50:46 +0100

    programando modelo useraddress

---
+++ models/UserAddress.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const UserAddressSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    address: [{
+        fullName: { type: String, required: true },
+        mobileNUmber: { type: Number, required: true },
+        pinCode: { type: Number, required: true },
+        locality: { type: String, required: true },
+        address: { type: String, require: true },
+        cityDistrictTown: { type: String, required: true },
+        state: { type: String, require: true },
+        lanmark: String,
+        alternatePhoneNumber: Number
+    }]
+})
+module.exports = mongoose.model('UserAddress', UserAddressSchema)
