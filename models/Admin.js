1d34f2e3f90acf7cdd08605881de7cc0346fdca8
tandavala <jose.tandavala@gmail.com>
2020-01-01 22:20:38 +0100

    programando o modelo admin

---
+++ models/Admin.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const AdminSchema = mongoose.Schema({
+    _id: mongoose.Schema.Schema.Types.ObjectId,
+    firstName: String,
+    lastName: String,
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdAt: Date
+})
+
+module.exports = mongoose.model('Admin'), AdminSchema
