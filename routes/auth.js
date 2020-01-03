b1a76f29cc3a2fe0ba8de9d930216d93f807e9a7
tandavala <jose.tandavala@gmail.com>
2020-01-03 07:55:50 +0100

    programando o motodo de login.

--- routes/auth.js
+++ routes/auth.js
@@ -18,3 +18,55 @@ routeer.get("/", auth, async (req, res) => {
     res.status(400).json({ msg: "User not found" });
   }
 });
+
+router.post(
+  "/",
+  [
+    check("email", "Por favor digite um email valido").isEmail(),
+    check("password", "A senha e obrigatoria").exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array()
+      });
+    }
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Credencial invalido" }] });
+      }
+      const isMath = await bcrypt.compare(password, user.password);
+
+      if (!isMath) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Credencial invalid" }] });
+      }
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Error no serdor");
+    }
+  }
+);
+
+module.exports = router;
